7ea2676b134ae79cecd33f16ffcde914143371af
aka <aka1w4@proton.me>
2026-06-24 23:06:40 +0700

    add config niri and waybar

--- .scripts/fix-mic.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set +e
-for c in /proc/asound/card*; do
-  CARD=$(basename "$c" | sed 's/card//')
-  amixer -c "$CARD" set 'Internal Mic Boost' 0 >/dev/null 2>&1
-  amixer -c "$CARD" set 'Headset Mic Boost' 0 >/dev/null 2>&1
-  amixer -c "$CARD" set 'Capture' 50% >/dev/null 2>&1
-done
-exit 0
