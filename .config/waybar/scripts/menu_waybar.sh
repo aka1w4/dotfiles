b62d51c49a32a72d72b71ba647f35a7a0c457f7f
aka <aka1w4@proton.me>
2026-07-14 13:13:34 +0700

    update waybar

--- .config/waybar/scripts/menu_waybar.sh
+++ .config/waybar/scripts/menu_waybar.sh
@@ -2,7 +2,7 @@
 
 CONFIG="$HOME/.config/waybar"
 if [[ $1 = "-m" ]]; then 
-  get=$(printf "waybar1\nwaybar2\nclear\n" | fuzzel --dmenu)
+  get=$(printf "waybar1\nwaybar2\nwaybar3\nclear\n" | fuzzel --dmenu)
 fi
 
 if pgrep waybar >/dev/null; then
@@ -15,7 +15,9 @@ case "$get" in
   ;;
   waybar2) 
     waybar &
-      ;;
+  ;;
+  waybar3) waybar -c "$CONFIG/alt/config_alt1.jsonc" &
+  ;;
   * | waybar1)
     waybar -c "$CONFIG/alt/config_alt.jsonc" -s "$CONFIG/alt/style_alt.css" &
   ;;
