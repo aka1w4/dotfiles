180991c5958fc454c4128d842626e7fb3f28bbf7
aka <aka1w4@proton.me>
2026-08-11 23:13:38 +0700

    nvim update

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,16 +1,18 @@
 require("nvchad.configs.lspconfig").defaults()
 
 local servers = {
-  "html",
-  "cssls",
+  "html-lsp",
+  --"cssls",
   "pyright",
   "gopls",
   "zls",
   "rust_analyzer",
   "clangd",
   "lua-language-server",
-  "cmake-language-server",
+  "neocmakelsp",
   "glsl_analyzer",
+  "vtsls",
+  "arduino-language-server"
 }
 
 vim.lsp.config.clangd = {
@@ -19,7 +21,22 @@ vim.lsp.config.clangd = {
     "--query-driver=/home/aka/.espressif/tools/**/bin/*",
     "--compile-commands-dir=build",
   },
+   
   filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
 }
+
+vim.lsp.config("arduino-language-server", {
+  cmd = {
+    "/usr/sbin/arduino-language-server",
+    "-clangd", "/usr/sbin/clangd",
+    "-cli", "/usr/sbin/arduino-cli",
+    "-cli-config", vim.fn.expand("$HOME/.arduino15/arduino-cli.yaml"),
+    "-fqbn", "esp32:esp32:esp32",
+  },
+
+  filetypes = { "arduino" },
+
+  root_markers = { "sketch.yaml", ".git" },
+})
 
 vim.lsp.enable(servers)
